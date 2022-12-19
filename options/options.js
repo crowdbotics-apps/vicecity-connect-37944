33513b4e6dbdc756afbc56c4d392fd24b554bda7
Crowdbotics <team@crowdbotics.com>
2022-12-19 23:10:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vicecity_connect_37944",
+  url: "https://vicecity_connect_37944.botics.co",
+  api: "https://vicecity_connect_37944.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
